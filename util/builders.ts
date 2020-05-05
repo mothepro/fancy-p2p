05381384709ecf7e9fd46e2dcc1985031b5e4b04
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-05-05 17:42:17 -0500

    Add builders and parsers for messages to be sent.

---
+++ util/builders.ts
@@ -0,0 +1,40 @@
+import { LobbyID, Name, Size, ClientID } from '@mothepro/signaling-lobby'
+
+/* Create ArrayBuffers to send to client and server. */
+
+const encoder = new TextEncoder
+
+export enum MessageType {
+  offer,
+  answer,
+  pranswer,
+  rollback,
+}
+
+/** Helper to create an Intro with the server. */
+export function buildIntro(lobby: LobbyID, name: Name) {
+  const nameBuffer = encoder.encode(name),
+    data = new DataView(new ArrayBuffer(Size.INT + nameBuffer.byteLength))
+  data.setInt32(0, lobby, true)
+  new Uint8Array(data.buffer, Size.INT).set(nameBuffer)
+  return data.buffer
+}
+
+/** Helper to build a group proposal or rejection. */
+export function buildProposal(accept: boolean, ...ids: ClientID[]) {
+  const data = new DataView(new ArrayBuffer(Size.CHAR + Size.SHORT * ids.length))
+  data.setInt8(0, +accept)
+  for (let i = 0; i < ids.length; i++)
+    data.setUint16(Size.CHAR + i * Size.SHORT, ids[i], true) 
+  return data.buffer
+}
+
+/** Sends a packed SDP to the server for rerouting. */
+export function buildSdp(to: ClientID, { type, sdp }: RTCSessionDescriptionInit) {
+  const sdpBuffer = encoder.encode(sdp),
+    data = new DataView(new ArrayBuffer(Size.SHORT + Size.CHAR + sdpBuffer.byteLength))
+  data.setUint16(0, to, true)
+  data.setUint8(Size.SHORT, MessageType[type!])
+  new Uint8Array(data.buffer, Size.CHAR + Size.SHORT).set(sdpBuffer)
+  return data.buffer
+}
---
+++ util/parsers.ts
@@ -0,0 +1,81 @@
+
+import { ClientID, Size, Code } from '@mothepro/signaling-lobby'
+import { MessageType } from './builders.js'
+
+/* Parse ArrayBuffers to sent from server to us. */
+
+const decoder = new TextDecoder
+
+/**
+ * Helper to gets a list of `ClientID`s from a buffer at an offset.
+ * 
+ * Unfortunately, Buffer -> UInt16Array is not WAI.
+ *  Also, do not rely on the underlying ArrayBuffer `data.buffer`, socket may modify it...
+ */
+export function parseClientIds(offset: number, data: DataView): ClientID[] {
+  const ids = []
+  for (let i = offset; i < data.byteLength; i += Size.SHORT)
+    ids.push(data.getUint16(i, true))
+  return ids
+}
+
+/** Helper to get status of a Client joining. */
+export function parseClientJoin(data: DataView) {
+  if (data.getUint8(0) == Code.CLIENT_JOIN
+    && data.byteLength > Size.CHAR + Size.SHORT)
+    return {
+      id: data.getUint16(Size.CHAR, true),
+      name: decoder.decode(data.buffer.slice(Size.CHAR + Size.SHORT)),
+    }
+
+  throw Error(`Expected a client join message, but got ${data}`)
+}
+
+/** Helper to get status of a Client leaving. */
+export function parseClientLeave(data: DataView) {
+  if (data.getUint8(0) == Code.CLIENT_LEAVE
+    && data.byteLength == Size.CHAR + Size.SHORT)
+    return data.getUint16(Size.CHAR, true)
+
+  throw Error(`Expected a client leave message, but got ${data}`)
+}
+
+/** Helper to get status of a Client leaving. */
+export function parseGroupChange(data: DataView) {
+  if ((data.getUint8(0) == Code.GROUP_REJECT || data.getUint8(0) == Code.GROUP_REQUEST)
+    && data.byteLength >= Size.CHAR + Size.SHORT
+    && data.byteLength % Size.SHORT == Size.CHAR)
+    return {
+      approve: data.getUint8(0) == Code.GROUP_REQUEST,
+      actor: data.getUint16(Size.CHAR, true),
+      members: parseClientIds(Size.CHAR, data),
+    }
+
+  throw Error(`Expected a group join or leave message, but got ${data}`)
+}
+
+
+/** Helper to get status of a Group finalization. */
+export function parseGroupFinalize(data: DataView) {
+  if (data.getUint8(0) == Code.GROUP_FINAL
+    && data.byteLength >= Size.CHAR + Size.INT)
+    return {
+      code: data.getInt32(Size.CHAR, true),
+      cmp: data.getUint16(Size.CHAR + Size.INT, true),
+      members: parseClientIds(Size.CHAR + Size.INT + Size.SHORT, data),
+    }
+
+  throw Error(`Expected a group finalize message, but got ${data}`)
+}
+
+export function parseSdp(data: DataView) {
+  if (data.byteLength > Size.SHORT + Size.CHAR
+    && data.getUint8(Size.CHAR) in MessageType)
+    return {
+      from: data.getUint16(0, true),
+      sdp: {
+        type: MessageType[data.getUint8(Size.SHORT)],
+        sdp: decoder.decode(data.buffer.slice(Size.SHORT + Size.CHAR)),
+      }
+    }
+}
