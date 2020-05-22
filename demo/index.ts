54342520cd124421d7f0a9012bfa7877f0908df7
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2020-05-21 19:53:44 -0500

    Override the form element in the index demo file

---
+++ demo/index.ts
@@ -0,0 +1,14 @@
+import { html, render } from 'lit-html'
+import './peer.js'
+
+const params = new URLSearchParams(location.search)
+
+// Add `lit-peer` element with the attributes if user has a name.
+if (params.has('name'))
+  render(html`
+    <lit-peer
+      name=${params.get('name')!}
+      retries=5
+      timeout=5000
+    ></lit-peer>`,
+    document.getElementById('log')!)
