847d9a58b48fd046772bc2539ef803bb05c0193b
Maurice Prosper <mothepro@live.com>
2021-02-17 00:12:07 -0600

    Actually export mock peer

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export type { SimpleClient as Client } from './src/Client.js'
-export type { MySimplePeer as Peer, MockPeer, Sendable } from './src/Peer.js'
+export type { MySimplePeer as Peer, Sendable } from './src/Peer.js'
 export { default, State } from './src/P2P.js'
+export { MockPeer } from './src/Peer.js'
 
 import type SimplePeer from 'simple-peer'
 import 'simple-peer'
